c5a651a99503040a2cf135970cd7e3ad1bd4c719
Axyonov Maxim <maksik.aksyonov@mail.ru>
2025-10-27 02:22:02 +0500

    Task6

---
+++ Tyuiu.AxyonovMA.Sprint3.Task6.V19.Test/Test1.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.AxyonovMA.Sprint3.Task6.V19.Lib;
+
+namespace Tyuiu.AxyonovMA.Sprint3.Task6.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumTheDivisors()
+        {
+            DataService ds = new DataService();
+            int startValue = 10;
+            int stopValue = 15;
+
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+            int wait = 45;
+
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.AxyonovMA.Sprint3.Task6.V19/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.AxyonovMA.Sprint3.Task6.V19.Lib;
+
+namespace Tyuiu.AxyonovMA.Sprint3.Task6.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ: Напишите программу, которая ищет среди целых чисел,            *");
+            Console.WriteLine("* принадлежащих числовому отрезку [10, 15] сумму всех делителей меньше 8  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 10;
+            int stopValue = 15;
+            Console.WriteLine(" Начало отрезка = " + startValue);
+            Console.WriteLine(" Конец отрезка = " + stopValue);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" Сумма всех делителей = " + ds.GetSumTheDivisors(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
