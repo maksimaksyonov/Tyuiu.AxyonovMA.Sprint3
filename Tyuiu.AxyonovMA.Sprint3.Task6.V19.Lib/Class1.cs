b243c15259eb60fd5ad50680f9b1d9d7e34e9f10
Axyonov Maxim <maksik.aksyonov@mail.ru>
2025-10-27 04:03:19 +0500

    Task6

--- Tyuiu.AxyonovMA.Sprint3.Task6.V19.Lib/Class1.cs
+++ Tyuiu.AxyonovMA.Sprint3.Task6.V19.Lib/Class1.cs
@@ -1,27 +1,25 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint3;
-
-namespace Tyuiu.AxyonovMA.Sprint3.Task6.V19.Lib
+﻿namespace Tyuiu.AxyonovMA.Sprint3.Task6.V19.Lib
 {
+    using tyuiu.cources.programming.interfaces.Sprint3;
+
     public class DataService : ISprint3Task6V19
     {
         public int GetSumTheDivisors(int startValue, int stopValue)
         {
-            int x;
-            int sum = 0;
-            for (x = startValue; x <= stopValue; x++)
+            int count = 0;
+
+            for (int i = startValue; i <= stopValue; i++)
             {
-                for (int d = 1; d <= x; d++)
+                for (int d = 1; d < 8; d++)
                 {
-                    if (x % d == 0)
+                    if (i % d == 0)
                     {
-                        if (d < 8)
-                        {
-                            sum += d;
-                        }
+                        count++;
                     }
                 }
             }
-            return sum;
+
+            return count;
         }
     }
 }
