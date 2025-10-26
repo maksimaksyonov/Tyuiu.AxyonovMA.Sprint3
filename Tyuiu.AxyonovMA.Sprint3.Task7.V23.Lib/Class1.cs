30a4365a91cc644c6979baecbe90abd5d7b6ce5f
Axyonov Maxim <maksik.aksyonov@mail.ru>
2025-10-27 02:25:45 +0500

    Task7

---
+++ Tyuiu.AxyonovMA.Sprint3.Task7.V23.Lib/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.AxyonovMA.Sprint3.Task7.V23.Lib
+{
+    public class DataService : ISprint3Task7V23
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = stopValue - startValue + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x == 1)
+                {
+                    y = 0;
+                    valueArray[count] = y;
+                    count++;
+                }
+                else
+                {
+                    y = (Math.Cos(x) + 2) / (2 * x - 2) - 2 * x + 4;
+                    y = Math.Round(y, 2);
+                    valueArray[count] = y;
+                    count++;
+                }
+
+
+            }
+            return valueArray;
+
+        }
+    }
+}
---
+++ Tyuiu.AxyonovMA.Sprint3.Task7.V23/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.AxyonovMA.Sprint3.Task7.V23.Lib;
+
+namespace Tyuiu.AxyonovMA.Sprint3.Task7.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Вывести таблицу значений функции на диапазоне [-5,5]                     *");
+
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("* y = 4-2x+(2+cos(x))/(2x-2)                                               *");
+            Console.WriteLine("****************************************************************************");
+
+
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            Console.WriteLine("+---------------------+---------------------+");
+            Console.WriteLine("+          X          +         f(x)        +");
+            Console.WriteLine("+---------------------+---------------------+");
+
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|        {0,5:d}        |        {1, 5:f2}        |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+---------------------+---------------------+");
+            Console.ReadKey();
+        }
+    }
+}
